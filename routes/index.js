fe8f75973c344a0764557503aa2291e16eaffe5f
Dylan <changliangdylan@gmail.com>
2023-09-29 23:18:31 -0400

    Added dynamic data, footer and header, CSS
    Added dynamic data importing it from data/portfolio.json and sending it to the res.render(). Added more partial CSS to the footer and header. The portfolio.json is a server-side data that is used for populating the template.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
+// I import the portfolio.json to have dynamic data so can be changed whenever I want.
+const portfolio = require('../data/portfolio.json')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { portfolio, title: "Home" });
 });
 
 module.exports = router;
